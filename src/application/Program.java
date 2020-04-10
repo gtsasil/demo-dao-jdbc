b6edfa8368d2f0917ab2ce8037354721c38e6852
gtsasil <gtsasil@gmail.com>
2020-04-10 01:26:59 +0100

    findAll impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -19,8 +20,7 @@ public class Program {
 		                               
 		System.out.println(seller);
 		
-		System.out.println("\n === TESTE 2: seller findByDepartment ===");
-		
+		System.out.println("\n === TESTE 2: seller findByDepartment ===");		
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller  obj : list) {
@@ -28,11 +28,16 @@ public class Program {
 		}
 		
 		System.out.println("\n === TESTE 3: seller findByDepartment ===");
-
 		list = sellerDao.findAll();
 		for(Seller  obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n === TESTE 4: seller findByDepartment ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
+	
 		
 	}
 
